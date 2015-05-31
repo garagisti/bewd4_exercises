b36aa61e58db3778728105008b322feccc3b3399
Shirren Premaratne <shirren@Sev.local>
2015-05-31 14:37:43 +1000

    Added math exercise to class 3

---
+++ class3/bewd_math.rb
@@ -0,0 +1,36 @@
+# This method adds two numbers and returns the
+# sum of the two numbers
+def add(x,y)
+  # TODO: Complete implementation
+end
+
+# This method calculates the difference of two numbers
+# and returns the difference of the two numbers
+def minus(x,y)
+  # TODO: Complete implementation
+end
+
+# This method calculates the product of two numbers
+# and returns the product of the two numbers
+def multiply(x,y)
+  # TODO: Complete implementation
+end
+
+# This method calculates the division of two numbers
+# and returns the division of the two numbers
+def divide(x,y)
+  # TODO: Complete implementation
+end
+
+puts 'Welcome to our simple calculator'
+puts 'Value of x: '
+first_value = gets.chomp
+
+puts 'Value of y: '
+second_value = gets.chomp
+
+# TODO: Finish this code below
+puts "Sum of x and y is "
+puts "Difference of x and y is "
+puts "Product of x and y is "
+puts "Division of x by y is "
