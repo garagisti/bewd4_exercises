ab078c3c3ef19c5b267115b9e2e3704ec393e2f0
Shirren Premaratne <shirren@Sev.local>
2015-06-04 14:48:21 +1000

    Added exercises for class 4

---
+++ class4/bewd_collections.rb
@@ -0,0 +1,21 @@
+characters = [
+  { name: 'Alfred Pennyworth', gender: :male },
+  { name: 'Selina Kyle', gender: :female },
+  { name: 'Harper Row', gender: :male },
+  { name: 'Tim Drake', gender: :male },
+  { name: 'Kate Kane', gender: :female },
+  { name: 'Barbara Gordon', gender: :female }
+]
+
+# Exercise 1
+# Print out all the names of the female characters
+
+# Exercise 2
+# Modify the original list of participants by separating the characters name
+# into two fields so for example name: would be replaced by first_name: and last_name:
+# i.e. { name: 'Alfred Pennyworth', gender: :male } becomes
+# { first_name: 'Alfred', last_name: 'Pennyworth', gender: :male }
+
+# Exercise 3
+# Calculate the number of male characters
+# male_participant_count = ?
